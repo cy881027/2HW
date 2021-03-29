992f341b28abec5f5b08ffff65fecffff1cabda5
瑩 佳 <a11910691110@gmail.com>
2021-03-30 02:05:20 +0800

    2或3的倍數加總

--- 2HW/Default.aspx.cs
+++ 2HW/Default.aspx.cs
@@ -8,7 +8,7 @@ using System.Web.UI.WebControls;
 namespace _2HW {
     public partial class Default : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
-
+            
         }
     }
 }
---
+++ 2HW/Prime.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _2HW
+{
+    public partial class Prime : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int N = 9;
+            int sum = 0;
+            Response.Write("2或3的倍數加總:"+ N +"</br>");
+            for (int i =1; i <= N; i++)
+            {
+                if((i % 2 ==0) || (i % 3 == 0))
+                {
+                    if ((i % 2 == 0) && (i % 3 == 0))
+                    {
+                        sum += i;
+                    }
+                    else
+                    {
+                        sum += i;
+                    }
+                }
+            }
+            Response.Write(sum);
+        }
+    }
+}
